c0e99e08f6222937f0207dcaded0d702379918e4
Harshad <harshad.chavda@techivies.com>
2022-08-04 18:56:19 +0530

    todo

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todolist',
@@ -6,18 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-  user=[{name:'string'}];
+  user=[''];
 
-  constructor() { }
+  constructor(private router:Router,private http:HttpClient) { }
   
   ngOnInit(): void {
+    this.user=[]
   }
-  
-  addData(uname: any) {
+  addData(uname: any ) {
     console.log(uname.value);
-      this.user.push({
-        name:uname.value
-      });
+      this.user.push(uname.value);
+        this.http.post('http://localhost:3000/User',uname.value)
+        .subscribe(res => {
+          alert("successfull");
+        });
+      
   }
   onDelete(item: number) {
     console.log(item);
